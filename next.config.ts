f897fbb5d6caa44c75b80a5cc06342f96b7d646d
BETINHOGAMESYOUTUBER <betinhogamesbr@gmail.com>
2026-08-04 17:30:47 -0300

    ci: adiciona automatizacao de deploy no GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+
+if (isGithubActions) {
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+  if (repoName) {
+    repo = `/${repoName}`;
+  }
+}
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/inicio' : '',
+  basePath: repo,
   images: {
     unoptimized: true,
   },
